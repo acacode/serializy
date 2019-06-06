3f21a686a45a8a37b8879df8c1a6f53559139a6e
Volkov Sergey <truefunrun@hotmail.com>
2019-06-06 23:52:15 +0300

    chore: changed sketche

--- example/index.ts
+++ example/index.ts
@@ -1,47 +1,43 @@
-import { createMapper, prop, StructProp } from '../src'
 
-// const Family = createMapper({
-//   Children: ['children', 'number'],
-// })
-
-// const cc = UserInfo.toClient({
-//   FirstName: 'Sergey',
-//   LastName: 'Volkov',
-// }) as any
-
-// console.log('cc.firstName', cc.firstName)
-
-createMapper({
-  age: prop().from('Age', 'float').to('float'),
-})
-
-const lol = [
-  new StructProp(['age', 'float'], ['Age','float']),
-  new StructProp(['firstName'], ['FirstName']),
-]
-
-console.log('lol', lol, createMapper)
-console.log('sas', lol[0].castTo('1234,5155', true))
-
-console.log(prop().from('Age', 'float').to('float'))
-
-class Creep {
-
+const ServerData = {
+  DeepInfo: {
+    BadHabits: ['Coffee', 'Development']
+  },
+  Family: {
+    ChildrenCount: 0,
+    Spouse: false,
+  },
+  FirstName: 'Sergey',
+  ID: '1CASSD@D#@Dd2d@2dDFC',
+  Job: {
+    Exp: 4,
+    Skills: ['JavaScript', 'ReactJS', 'NodeJS']
+  },
+  LastName: 'Volkov',
 }
 
-class Monster {
-  public hp = prop().from('HP', 'float')
-  public level = prop().from('Level', 'integer')
+const ClientData = {
+  badHabits: ['Coffee', 'Development'],
+  family: {
+    childCount: 0,
+    spouse: false,
+  },
+  firstName: 'Sergey',
+  id: '1CASSD@D#@Dd2d@2dDFC',
+  job: {
+    experience: 4,
+    skills: ['JavaScript', 'ReactJS', 'NodeJS']
+  },
+  lastName: 'Volkov',
+}
 
-  public fullName = prop()
-    .from((model) => `${model.FirstName} ${model.LastName}`)
-    .to((model) => {
-      const [FirstName, LastName] = model.fullName.split(' ')
-      return {
-        FirstName,
-        LastName
-      }
-    })
+const structProp = (a: any) => {}
 
-  public creeps = prop('array').of(Creep)
+// class FamilyInfo {
+//   public childCount = structProp()
+// }
+
+class Profile {
+  public badHabits = structProp(({ DeepInfo }) => DeepInfo.BadHabits)
+  public family = structProp(({ DeepInfo }) => DeepInfo.BadHabits)
 }
