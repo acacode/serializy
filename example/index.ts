da778c327ac056d9a2f5113380dcc1bda6c0bf4f
Volkov Sergey <truefunrun@hotmail.com>
2019-06-29 11:30:12 +0300

    fix: remove setInterval from examples

--- example/index.ts
+++ example/index.ts
@@ -31,32 +31,30 @@ const ServerData = {
   LastName: 'Volkov',
 }
 
-setInterval(() => {
-
   // const FamilyModel = model(class FamilyModel {
   //   childCount = field('ChildrenCount', 'number', 'string')
   //   spouse = field('Spouse', 'boolean')
   // })
 
-  class ProfileDeclaration {
-    badHabits = field(
+class ProfileDeclaration {
+  badHabits = field(
       ({ DeepInfo }: any) => DeepInfo.BadHabits,
       ({ badHabits }) => ({ DeepInfo: { BadHabits: badHabits } })
     )
-    family = field('Family', {
-      childCount: field('ChildrenCount', 'number', 'string'),
-      spouse: field('Spouse', 'boolean')
-    })
-    id = field('ID')
-    job = field('Job', {
-      experience: field('Exp', 'integer'),
-      skills: fieldArray('Skills', 'string')
-    })
-    languages = fieldArray('Languages', {
-      id: field('ID', 'string'),
-      name: field('Name', 'string')
-    })
-    personalInfo = field(
+  family = field('Family', {
+    childCount: field('ChildrenCount', 'number', 'string'),
+    spouse: field('Spouse', 'boolean')
+  })
+  id = field('ID')
+  job = field('Job', {
+    experience: field('Exp', 'integer'),
+    skills: fieldArray('Skills', 'string')
+  })
+  languages = fieldArray('Languages', {
+    id: field('ID', 'string'),
+    name: field('Name', 'string')
+  })
+  personalInfo = field(
       ({ FirstName, LastName }) => ({
         firstName: FirstName,
         fullName: `${FirstName} ${LastName}`,
@@ -68,44 +66,44 @@ setInterval(() => {
       })
     )
 
-    getFullName = () => (this.personalInfo as any).firstName
-  }
+  getFullName = () => (this.personalInfo as any).firstName
+}
 
-  const ProfileModel = model(ProfileDeclaration)
+const ProfileModel = model(ProfileDeclaration)
 
-  const profile = new ProfileModel(ServerData)
+const profile = new ProfileModel(ServerData)
 
-  profile.personalInfo.firstName = 'Sergey'
-  profile.personalInfo.lastName = 'Volkov'
+profile.personalInfo.firstName = 'Sergey'
+profile.personalInfo.lastName = 'Volkov'
 
-  profile.id = `${profile.id}_CHANGED`
+profile.id = `${profile.id}_CHANGED`
 
-  console.log(profile.deserialize())
+console.log(profile.deserialize())
 
-  const OddModel = model({
-    prop1: field('Prop')
-  })
+const OddModel = model({
+  prop1: field('Prop')
+})
 
-  console.log(new OddModel({ Prop: 'blabla ' }))
+console.log(new OddModel({ Prop: 'blabla ' }))
 
-  class AnimalD {
-    age = field('Age', 'number')
-    name = field('Name', 'string')
-  }
+class AnimalD {
+  age = field('Age', 'number')
+  name = field('Name', 'string')
+}
 
-  class DogD extends AnimalD {
-    breed = field('Breed', 'string')
-  }
+class DogD extends AnimalD {
+  breed = field('Breed', 'string')
+}
 
-  const DogModel = model(DogD)
+const DogModel = model(DogD)
 
-  const dog = new DogModel({
-    Age: 4,
-    Breed: 'scottish terrier',
-    Name: 'Fluffy'
-  })
+const dog = new DogModel({
+  Age: 4,
+  Breed: 'scottish terrier',
+  Name: 'Fluffy'
+})
 
-  console.log(dog)
+console.log(dog)
   /*
   {
     age: 4,
@@ -113,5 +111,3 @@ setInterval(() => {
     name: 'Fluffy'
   }
   */
-
-}, 2000)
