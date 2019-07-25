011bda6228c4b6e65206572541cf8e5f969deb0a
Sergey Volkov <sergey.volkov@firstlinesoftware.com>
2019-07-25 12:01:54 +0300

    internal: added typings for further

--- src/global_types.ts
+++ src/global_types.ts
@@ -3,3 +3,22 @@ export type ValueOf<T> = T[keyof T]
 export interface AllKeysAre<T> {
   [key: string]: T
 }
+
+export interface Dictionary<T> {
+  [index: string]: T
+}
+export interface NumericDictionary<T> {
+  [index: number]: T
+}
+
+export type AnyKindOfDictionary<T= unknown> = Dictionary<T> | NumericDictionary<T>
+
+export type Many<T> = T | ReadonlyArray<T>
+
+export type PropertyName = string | number | symbol
+
+// TODO: use it
+export type DDD<T extends AnyKindOfDictionary> = (
+  object: T | null | undefined,
+  ...paths: Array<Many<PropertyName>>
+) => T
